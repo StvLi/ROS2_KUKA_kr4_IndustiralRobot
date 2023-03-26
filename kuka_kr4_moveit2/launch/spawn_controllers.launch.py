ad12efd1fd99878efaa4d8462e0c505bfb91ffa9
StvLi <3190102290@zju.edu.cn>
2023-03-26 06:40:07 -0400

    修复了之前没有添加moveit_controller所造成的bug

---
+++ kuka_kr4_moveit2/launch/spawn_controllers.launch.py
@@ -0,0 +1,7 @@
+from moveit_configs_utils import MoveItConfigsBuilder
+from moveit_configs_utils.launches import generate_spawn_controllers_launch
+
+
+def generate_launch_description():
+    moveit_config = MoveItConfigsBuilder("kuka_kr4_description", package_name="kuka_kr4_moveit2").to_moveit_configs()
+    return generate_spawn_controllers_launch(moveit_config)
